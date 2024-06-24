d980fd1a07849828c4b522737c9f9560573bc6e4
Markell <85594945+Markell@users.noreply.github.com>
2024-06-25 01:47:13 +0300

    Changed data saving method

--- main.py
+++ main.py
@@ -4,39 +4,36 @@ from rich.console import Console
 
 console = Console()
 
-path = '0_localsave.json'
-with open(path, 'r') as infile:  # Opens the json file
-    jsn = json.load(infile)  # Reads the file as json
-flags = jsn["flags"]  # Grabs the "flags" list from the json
+file_path = '0_localsave.json'
+with open(file_path, 'r', encoding="utf-8") as savefile:  # Opens the json file
+    data = json.load(savefile)  # Reads the file as json
+flags = data["flags"]  # Grabs the "flags" list from the json
+
 
 # "Your save file should be located at: C:\Program Files (x86)\Steam\userdata\[Number]\632360\remote\UserProfiles"
-
-
 def manage_characters():
-    unlocked_char_index = []
-    unlocked_char_value = []
-    characters = ['acrid', 'arti', 'bandit', 'loader', 'hand', 'mercenary', 'pilot', 'sniper', 'enforcer', 'drifter',
-                  'engineer', 'chef', 'miner', 'robomando']
-
-    new_characters = {
-        'acrid': "Acrid",
-        'arti': "Arti",
-        'bandit': "Bandit",
-        'loader': "Loader",
-        'hand': "Hand",
-        'mercenary': "Mercenary",
-        'pilot': "Pilot",
-        'sniper': "Sniper",
-        'enforcer': "Enforcer",
-        'drifter': "Drifter",
-        'engineer': "Engineer",
-        'chef': "Chef",
-        'miner': "Miner"
+    characters = {
+        "Acrid": 'acrid',
+        "Arti": 'arti',
+        "Bandit": 'bandit',
+        "Loader": 'loader',
+        "Hand": 'hand',
+        "Mercenary": 'mercenary',
+        "Pilot": 'pilot',
+        "Sniper": 'sniper',
+        "Enforcer": 'enforcer',
+        "Drifter": 'drifter',
+        "Engineer": 'engineer',
+        "Chef": 'chef',
+        "Miner": 'miner'
     }
 
+    unlocked_char = {}
+
     # Creates the formating for the json of the characters
-    def format_name(character):
-        return f'challenge_unlock_{character}_completed'
+    def format_name(name):
+        name = characters.get(name)
+        return f'challenge_unlock_{name}_completed'
 
     # Check to see what is unlocked and what is not
     def check_char_name():
@@ -44,33 +41,27 @@ def manage_characters():
             form_name = format_name(name)
 
             if form_name in flags:
-                unlocked_char_index.append(characters.index(name))
-                unlocked_char_value.append(name)
+                unlocked_char[name] = list(characters).index(name)
 
     check_char_name()
 
     console.print("Manage game characters")
-
     # Choose multiple options from a list
-    char_to_edit = select_multiple(characters, tick_style='green', cursor_style='green',
-                                   ticked_indices=unlocked_char_index)
+    char_list = select_multiple(list(characters.keys()), tick_style='green', cursor_style='green',
+                                ticked_indices=list(unlocked_char.values()))
 
-    if char_to_edit:  # Check if selection is not empty (esc key pressed)
-        for char_name in char_to_edit:
-            if char_name not in unlocked_char_value:
-                flags.append(format_name(char_name))
+    for char_name in char_list:
+        if char_name not in unlocked_char.keys():
+            flags.append(format_name(char_name))
 
-        for char_name in unlocked_char_value:
-            if char_name not in char_to_edit:
-                flags.remove(format_name(char_name))
+    for char_name in unlocked_char.keys():
+        if char_name not in char_list:
+            flags.remove(format_name(char_name))
 
-        json_data = json.dumps(jsn, separators=(',', ':'))
-        open(path, 'w').write(json_data)
+    print(char_list)
 
-        # Remove spaces that are not there in the save file before
-        with_spaces = open(path, 'r').read()
-        without_spaces = with_spaces.replace(' ', '')
-        open(path, 'w').write(without_spaces)
+    with open(file_path, "w", encoding="utf-8") as mod_savefile:
+        json.dump(data, mod_savefile, separators=(',', ':'))
 
 
 def manage_abilities():
@@ -138,7 +129,6 @@ while menu_choice:
     elif menu_choice == "Exit":
         console.clear()
         break
-
 console.clear()
 print(logo)
 input("                            Press Enter to continue...")
